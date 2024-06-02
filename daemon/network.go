moby/moby

libnet: remove the `EndpointInfo` interface
`EndpointInfo` was introduced in moby/libnetwork@d8ba1e231. The commit's
message states:

> This sort of deisgn hides the libnetwork specific type details from
> drivers.

9 years later it turns out the `Endpoint` struct is the only implementor
of that interface, and `libnetwork` and `daemon` packages are the only
consumers. This is adding an unneeded level of indirection.

`(*Endpoint).Info()` is the only function that returns an `EndpointInfo`.
It's basically 12 lines of cursed indirection packed together. But, as
noted by thaJeztah in 210abfa, we need to make sure the `*Endpoint`
receiver is always fully hydrated to get rid of it. So here we go.

As the diff shows, most of the time `(*Endpoint).Info()` is called right
after a call to `(*Network).Endpoints()` (sometimes through an
indirection). That method in turns calls `getEndpointsFromStore`, which
is calling `(*Store).List()`, which is either retrieving all objects
of a given type from its internal cache (if it was populated), or
retrieve them all from boltdb. In that case, `*Endpoint`s are fully
hydrated.

- `disconnectFromNetwork`: calls `WalkEndpoints`, which calls
  `(*Network).Endpoints()`, right before ;
- `buildContainerAttachments`: calls `(*Network).Endpoints()` right before ;
- `buildEndpointResource`: `ep` and `info` are actually both the same thing ;
- `clearAttachableNetworks`: calls `(*Network).Endpoints()` right before ;
- `findLBEndpointSandbox`: calls `(*Network).Endpoints()` right before ;
- `addLBBackend`: calls `(*Network).Endpoints()` right before ;
- `deleteLoadBalancerSandbox`: calls `(*Network).EndpointByName()`, which
  calls `WalkEndpoint` (see above).

We're left with the following, less obvious cases:

- `updateJoinInfo`: called from `(*Daemon).connectToNetwork()`, where
  `*Endpoint`s are created ;
- `buildEndpointInfo`: called from `(*Daemon).updateEndpointNetworkSettings()`,
  again called from `(*Daemon).connectToNetwork()` ;

Meaning,

daemon/network.go
@@ -632,15 +632,11 @@ func buildNetworkResource(nw *libnetwork.Network) types.NetworkResource {
 // detailed mode.
 func buildContainerAttachments(nw *libnetwork.Network) map[string]types.EndpointResource {
 	containers := make(map[string]types.EndpointResource)
-	for _, e := range nw.Endpoints() {
-		ei := e.Info()
-		if ei == nil {
-			continue
-		}
-		if sb := ei.Sandbox(); sb != nil {
-			containers[sb.ContainerID()] = buildEndpointResource(e, ei)
+	for _, ep := range nw.Endpoints() {
+		if sb := ep.Sandbox(); sb != nil {
+			containers[sb.ContainerID()] = buildEndpointResource(ep)
 		} else {
-			containers["ep-"+e.ID()] = buildEndpointResource(e, ei)
+			containers["ep-"+ep.ID()] = buildEndpointResource(ep)
 		}
 	}
 	return containers
@@ -756,12 +752,12 @@ func buildIPAMResources(nw *libnetwork.Network) network.IPAM {
 
 // buildEndpointResource combines information from the endpoint and additional
 // endpoint-info into a [types.EndpointResource].
-func buildEndpointResource(ep *libnetwork.Endpoint, info libnetwork.EndpointInfo) types.EndpointResource {
+func buildEndpointResource(ep *libnetwork.Endpoint) types.EndpointResource {
 	er := types.EndpointResource{
 		EndpointID: ep.ID(),
 		Name:       ep.Name(),
 	}
-	if iface := info.Iface(); iface != nil {
+	if iface := ep.Iface(); iface != nil {
 		if mac := iface.MacAddress(); mac != nil {
 			er.MacAddress = mac.String()
 		}
@@ -783,11 +779,7 @@ func (daemon *Daemon) clearAttachableNetworks() {
 			continue
 		}
 		for _, ep := range n.Endpoints() {
-			epInfo := ep.Info()
-			if epInfo == nil {
-				continue
-			}
-			sb := epInfo.Sandbox()
+			sb := ep.Sandbox()
 			if sb == nil {
 				continue
 			}
@@ -1039,12 +1031,6 @@ func buildEndpointInfo(networkSettings *internalnetwork.Settings, n *libnetwork.
 		return errors.New("network cannot be nil")
 	}
 
-	epInfo := ep.Info()
-	if epInfo == nil {
-		// It is not an error to get an empty endpoint info
-		return nil
-	}
-
 	nwName := n.Name()
 	if _, ok := networkSettings.Networks[nwName]; !ok {
 		networkSettings.Networks[nwName] = &internalnetwork.EndpointSettings{
@@ -1054,7 +1040,7 @@ func buildEndpointInfo(networkSettings *internalnetwork.Settings, n *libnetwork.
 	networkSettings.Networks[nwName].NetworkID = n.ID()
 	networkSettings.Networks[nwName].EndpointID = ep.ID()
 
-	iface := epInfo.Iface()
+	iface := ep.Iface()
 	if iface == nil {
 		return nil
 	}

akerouanton/docker

daemon/network.go
@@ -630,15 +630,11 @@ func buildNetworkResource(nw *libnetwork.Network) types.NetworkResource {
 // detailed mode.
 func buildContainerAttachments(nw *libnetwork.Network) map[string]network.EndpointResource {
 	containers := make(map[string]network.EndpointResource)
-	for _, e := range nw.Endpoints() {
-		ei := e.Info()
-		if ei == nil {
-			continue
-		}
-		if sb := ei.Sandbox(); sb != nil {
-			containers[sb.ContainerID()] = buildEndpointResource(e, ei)
+	for _, ep := range nw.Endpoints() {
+		if sb := ep.Sandbox(); sb != nil {
+			containers[sb.ContainerID()] = buildEndpointResource(ep)
 		} else {
-			containers["ep-"+e.ID()] = buildEndpointResource(e, ei)
+			containers["ep-"+ep.ID()] = buildEndpointResource(ep)
 		}
 	}
 	return containers
@@ -754,12 +750,12 @@ func buildIPAMResources(nw *libnetwork.Network) network.IPAM {
 
 // buildEndpointResource combines information from the endpoint and additional
 // endpoint-info into a [types.EndpointResource].
-func buildEndpointResource(ep *libnetwork.Endpoint, info libnetwork.EndpointInfo) network.EndpointResource {
+func buildEndpointResource(ep *libnetwork.Endpoint) network.EndpointResource {
 	er := network.EndpointResource{
 		EndpointID: ep.ID(),
 		Name:       ep.Name(),
 	}
-	if iface := info.Iface(); iface != nil {
+	if iface := ep.Iface(); iface != nil {
 		if mac := iface.MacAddress(); mac != nil {
 			er.MacAddress = mac.String()
 		}
@@ -781,11 +777,7 @@ func (daemon *Daemon) clearAttachableNetworks() {
 			continue
 		}
 		for _, ep := range n.Endpoints() {
-			epInfo := ep.Info()
-			if epInfo == nil {
-				continue
-			}
-			sb := epInfo.Sandbox()
+			sb := ep.Sandbox()
 			if sb == nil {
 				continue
 			}
@@ -1037,12 +1029,6 @@ func buildEndpointInfo(networkSettings *internalnetwork.Settings, n *libnetwork.
 		return errors.New("network cannot be nil")
 	}
 
-	epInfo := ep.Info()
-	if epInfo == nil {
-		// It is not an error to get an empty endpoint info
-		return nil
-	}
-
 	nwName := n.Name()
 	if _, ok := networkSettings.Networks[nwName]; !ok {
 		networkSettings.Networks[nwName] = &internalnetwork.EndpointSettings{
@@ -1052,7 +1038,7 @@ func buildEndpointInfo(networkSettings *internalnetwork.Settings, n *libnetwork.
 	networkSettings.Networks[nwName].NetworkID = n.ID()
 	networkSettings.Networks[nwName].EndpointID = ep.ID()
 
-	iface := epInfo.Iface()
+	iface := ep.Iface()
 	if iface == nil {
 		return nil
 	}